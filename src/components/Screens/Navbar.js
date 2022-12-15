037a0c4b7dc65cb0bfafaba04d260cf4ee25808b
devamanin <amansingh1032000@gmail.com>
2022-12-16 01:01:03 +0530

    Added bottom navBar

--- src/components/Screens/Navbar.js
+++ src/components/Screens/Navbar.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { useNavigate, Outlet } from 'react-router-dom'
+import { useState } from 'react'
+import LowerNav from './lowerNav/LowerNav'
+
+
 
 const Navbar = () => {
   return (
     <div>
       <div>Code</div>
       <div className="h-full overflow-auto">
+        <LowerNav/>
         <Outlet />
       </div>
     </div>
