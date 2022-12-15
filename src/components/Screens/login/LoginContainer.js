8d5730ecd2e29b8e2be0d64e7c739f43755add9a
devamanin <amansingh1032000@gmail.com>
2022-12-16 01:30:35 +0530

    Initial commit

---
+++ src/components/Screens/login/LoginContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './LoginContainer.css'
+
+export default function LoginContainer() {
+  return (
+    <>
+        <div className='cover'></div>
+        <div className='container'></div>
+    </>
+  )
+}
