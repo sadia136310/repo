d7550049641dbf9b2e585643455a49e20d9f95c7
mursona <mursona>
2022-12-24 13:50:49 +0600

    cart section design and product filter work category name ways and details product page

---
+++ src/components/Screens/ProductCategory/Data.js
@@ -0,0 +1,263 @@
+const Data = [
+ 
+    {
+        "id": 1,
+        "category": "Uncategorized",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/WhatsApp-Image-2021-11-12-at-17.12.32-450x450.jpeg",
+        "title": "UNCATEGORIZED"
+    },
+    {
+        "id": 2,
+        "category": "Popular",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/IMG-20221109-WA0035-1-450x450.jpg",
+        "title": "FRAME",
+        "desc": "1 Photo Best Wishes Photo Frame 8 Inch by 7 Inch",
+        "previous_price": "₹399.00",
+        "current_price": "₹229.00"
+    },
+    {
+        "id": 3,
+        "category": "Popular",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/IMG-20221109-WA0036-450x450.jpg",
+        "title": "POPULAR",
+        "desc": "1 Photo Birthday Wish Photo Frame 8 inch by 7 inch",
+        "previous_price": "₹399.00",
+        "current_price": "₹229.00"
+
+    },
+    {
+        "id": 4,
+        "category": "Popular",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/IMG-20221109-WA0040-450x450.jpg",
+        "title": "POPULAR",
+        "desc": "11 Photos Customized Clock",
+        "previous_price": "₹1,499.00",
+        "current_price": "₹999.00"
+
+    },
+    {
+        "id": 5,
+        "category": "By Occasion",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/IMG-20221109-WA0030-450x450.jpg",
+
+        "title": "HOME AND LIVING",
+        "desc": "12 Photos Memories Photo Frame 16*16",
+        "previous_price": "₹1,569.00",
+        "current_price": "₹1,199.00"
+    },
+    {
+        "id": 6,
+        "category": "Popular",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/IMG-20221109-WA0033-450x450.jpg",
+        "title": "POPULAR",
+
+        "desc": " 4 Heart Shaped Photo Round Couple Frame",
+        "previous_price": "₹549.00",
+        "current_price": "₹499.00"
+    },
+    {
+        "id": 7,
+        "category": "Popular",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/IMG-20221109-WA0045-450x450.jpg",
+
+        "title": "FRAME",
+        "desc": "4 Photos Trending Birthday Photo Frame",
+        "previous_price": "₹1,299.00",
+        "current_price": "₹899.00"
+
+
+    },
+    {
+        "id": 8,
+        "category": "Popular",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/IMG-20221109-WA0031-450x450.jpg",
+        "title": "POPULAR",
+        "desc": "5 Photo Couple Frame",
+        "previous_price": "₹689.00",
+        "current_price": "₹489.00"
+    },
+    {
+        "id": 9,
+        "category": "Popular",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/IMG-20221109-WA0034-450x450.jpg",
+        "title": "POPULAR",
+        "desc": "5 Photos Toghether Forever Photo Frame",
+        "previous_price": "₹549.00",
+        "current_price": "₹499.00"
+
+    },
+    {
+        "id": 10,
+        "category": "Popular",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/IMG-20221109-WA0042-450x450.jpg",
+        "title": "POPULAR",
+        "desc": "6 Photos New Trending Anniversary Photo Frame",
+        "previous_price": "₹499.00",
+        "current_price": "₹299.00"
+
+    },
+    {
+        "id": 11,
+        "category": "Popular",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/IMG-20221109-WA0037-450x450.jpg",
+        "title": "FRAME",
+        "desc": "Best Couple Photo Frame",
+        "previous_price": "₹399.00",
+        "current_price": "₹229.00"
+
+
+    },
+    {
+        "id": 12,
+        "category": "Clutch",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/Name-clutch-bhavin-300x300-1-450x450.jpg",
+        "title": "ANNIVERSARY",
+        "desc": "Clutch Bag"
+
+        
+    },
+    {
+        "id": 13,
+        "category": "By Occasion",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/custom-mouse-pad-personalized-mouse-mats-your-own-design-1-450x450.jpg",
+        "title": "POPULAR",
+        "desc": "Customized Mouse Pad",
+        "previous_price": "₹249.00",
+        "current_price": "₹189.00"
+    },
+    {
+        "id": 14,
+        "category": "By Occasion",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/WhatsApp-Image-2022-02-03-at-06.46.40-450x450.jpeg",
+        "title": "NEON LIGHT",
+        "desc": "Neon Customize Name with 4 letter (7×15 inch)"
+    },
+    {
+        "id": 15,
+        "category": "By Occasion",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/WhatsApp-Image-2021-12-23-at-07.10.31-1-450x450.jpeg",
+        "title": "NEON LIGHT",
+        "desc": "Neon Light"
+    },
+    {
+        "id": 16,
+        "category": "By Occasion",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/WhatsApp-Image-2021-12-30-at-06.03.48-450x450.jpeg",
+        "title": "NEON LIGHT",
+        "desc": "Neon Light"
+    },
+    {
+        "id": 17,
+        "category": "By Occasion",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/WhatsApp-Image-2022-01-13-at-22.10.47-450x450.jpeg",
+        "title": "NEON LIGHT",
+        "desc": "Neon Light"
+
+    },
+    {
+        "id": 18,
+        "category": "By Occasion",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/WhatsApp-Image-2022-02-28-at-14.33.34-450x450.jpeg",
+        "title": "NEON LIGHT",
+        "desc": "Neon Light"
+
+
+    },
+    {
+        "id": 19,
+        "category": "By Occasion",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/WhatsApp-Image-2022-03-01-at-04.32.52-450x450.jpeg",
+        "title": "NEON LIGHT",
+        "desc": "Neon Light"
+
+
+
+    },
+    {
+        "id": 20,
+        "category": "By Occasion",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/WhatsApp-Image-2021-09-18-at-00.20.45-450x450.jpeg",
+        "title": "NEON LIGHT",
+        "desc": "Neon light with 1 letter",
+        "previous_price": "₹1,299.00",
+        "current_price": "₹1,199.00"
+
+
+
+    },
+    {
+        "id": 21,
+        "category": "By Occasion",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/WhatsApp-Image-2022-02-08-at-03.11.37-450x450.jpeg",
+        "title": "NEON LIGHT",
+        "desc": "Neon Light With 2 Names Upto 6 Letters Each Name (12×18) Inch"
+    },
+    {
+        "id": 22,
+        "category": "By Occasion",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/WhatsApp-Image-2021-11-10-at-14.27.25-450x450.jpeg",
+        "title": "NEON LIGHT",
+        "desc": "Neon light with 5/6 letter(7×18 inch)"
+
+    },
+    {
+        "id": 23,
+        "category": "By Occasion",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/WhatsApp-Image-2022-01-18-at-23.56.32-450x450.jpeg",
+        "title": "NEON LIGHT",
+        "desc": "Neon light with 7 letter (7×18)inch"
+
+    },
+    {
+        "id": 24,
+        "category": "Uncategorized",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/WhatsApp-Image-2021-09-28-at-04.23.13-1-450x450.jpeg",
+        "title": "UNCATEGORIZED",
+        "desc": "Neon light with 7 letter (7×18)inch"
+
+
+    },
+    {
+        "id": 25,
+        "category": "Popular",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/IMG-20221109-WA0037-450x450.jpg",
+        "title": "POPULAR",
+        "desc": "Trending 4 Photo Rectangular Couple Frame Velvet Finish",
+        "previous_price": "₹1,299.00",
+        "current_price": "₹999.00"
+    },
+    {
+        "id": 26,
+        "category": "By Gender",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/IMG-20221109-WA0039-450x450.jpg",
+        "title": "FASHION AND LIFESTYLE",
+        "desc": "Your Name Customized Leather Ladies Wallet",
+        "previous_price": "₹249.00",
+        "current_price": "₹199.00"
+
+    },
+    {
+        "id": 27,
+        "category": "Popular",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/IMG-20221109-WA0044-450x450.jpg",
+        "title": "POPULAR",
+        "desc": "Your Name Customized Wallet, Glass Case, Key Ring Hamper",
+        "previous_price": "₹699.00",
+        "current_price": "₹699.00"
+
+
+    },
+    {
+        "id": 28,
+        "category": "Popular",
+        "img": "https://stunhaul.com/wp-content/uploads/2022/11/IMG-20221109-WA0043-450x450.jpg",
+        "title": "FRAME",
+        "desc": "6 Photos Best Couple Frame",
+        "previous_price": "₹499.00",
+        "current_price": "₹299.00"
+    }
+
+
+  ];
+   
+  export default Data;
